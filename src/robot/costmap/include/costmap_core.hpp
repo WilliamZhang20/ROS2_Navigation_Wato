e21d8346238600b91ef36d4c5da9da5e154950c7
Bill <wzhang20@yahoo.com>
2025-09-21 12:40:12 -0400

    finished costmap

--- src/robot/costmap/include/costmap_core.hpp
+++ src/robot/costmap/include/costmap_core.hpp
@@ -11,9 +11,21 @@ class CostmapCore {
     // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
     explicit CostmapCore(const rclcpp::Logger& logger);
 
+    void initializeCostmap(int width, int height, double resolution);
+    bool convertToGrid(double x, double y, int &x_grid, int &y_grid);
+    void markObstacle(int x_grid, int y_grid);
+
+    int getWidth() const { return width_; }
+    int getHeight() const { return height_; }
+    double getResolution() const { return resolution_; }
+    std::vector<int8_t>& data() { return costmap_; }
+      
   private:
     rclcpp::Logger logger_;
-
+    int width_;
+    int height_;
+    double resolution_;
+    std::vector<int8_t> costmap_;
 };
 
 }  
--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -3,20 +3,26 @@
  
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
- 
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "costmap_core.hpp"
  
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
-    
-    // Place callback function here
-    void publishMessage();
- 
+
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+
   private:
+    void inflateObstacles();
+    void inflateObstacles();
+
+    void publishCostmap();
+
     robot::CostmapCore costmap_;
     // Place these constructs here
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
  
